b2dc723c69aac61ae05dba37e9f04a00c1551077
nicho <nicholaspangts@gmail.com>
2025-03-21 21:40:55 +0800

    Added Font and updated yaml file

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,8 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
