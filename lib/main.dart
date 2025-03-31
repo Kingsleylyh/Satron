f4117834a419dead4d892378add69f81a5f71566
nicho <nicholaspangts@gmail.com>
2025-03-31 09:36:10 +0800

    Added Font and updated yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:satron/pages/home.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(options:
+    FirebaseOptions(apiKey: ('${dotenv.env["FIREBASE_APIKEY"]}'),
+        appId: ('${dotenv.env["FIREBASE_APPID"]}'),
+        messagingSenderId: ('${dotenv.env["FIREBASE_MESSAGINGSENDERID"]}'),
+        projectId: ('${dotenv.env["FIREBASE_PROJECTID"]}')));
+  }
+
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
